e6b32e9382ab82bed5269f8142f909176143bb8c
Kelly M Syp <tskms@wakefern.com>
2020-02-23 15:11:20 -0500

    Added Tilemap and player mobility

---
+++ Scripts/Movement/camera_follow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera_follow : MonoBehaviour {
+	
+	public Transform target;
+	Camera mycam;
+
+	// Use this for initialization
+	void Start () {
+		mycam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mycam.orthographicSize = (Screen.height/100f)/2f;
+		
+		if(target){
+			transform.position = Vector3.Lerp(transform.position, target.position, 0.1f) + new Vector3(0,0,-.5f);
+		}
+	}
+}
---
+++ Scripts/Movement/character_Movement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class character_Movement : MonoBehaviour {
+	
+	Rigidbody2D rbody;
+	//Animator anim;
+	public int speed = 1;
+
+	public float minX = 0.0f;
+	public float maxX = 10.0f;
+
+	void Start () {
+		rbody = GetComponent<Rigidbody2D>();
+		//anim = GetComponent<Animator>();
+
+	}
+	
+	void Update () {
+
+		if (GameManager.canMove) {
+			Vector2 movement_vector = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		
+			if (movement_vector != Vector2.zero) {
+				//anim.SetBool ("isWalking", true);
+				//anim.SetFloat ("Input_x", movement_vector.x);
+				//anim.SetFloat ("Input_y", movement_vector.y);
+			} else {
+				//anim.SetBool ("isWalking", false);
+			}
+
+			//Mathf.Clamp (rbody.transform.position.x, minX, maxX);
+
+		
+			rbody.MovePosition (rbody.position + movement_vector * Time.deltaTime * speed);
+		}
+	}
+}
